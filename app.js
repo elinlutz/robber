80acff11abcc353edda6e315c42a90f38e576df1
Elin Lütz <elin@freya.se>
2019-04-12 00:03:29 +0200

    Folder changes

---
+++ app.js
@@ -0,0 +1,33 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const logger = require('koa-logger')
+const koaBody = require('koa-body')
+
+const encoder = require('./src/encoder')
+const decoder = require('./src/decoder')
+
+const app = new Koa()
+const router = new Router()
+
+app.use(logger())
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+router.get('/', (ctx) => {
+  ctx.body = 'Hello World!'
+})
+
+router.post('/encode', koaBody(), (ctx) => {
+  const body = ctx.request.body
+  if (!body.input) ctx.throw(400, '.input is required')
+  ctx.body = encoder.encode(body.input)
+})
+
+router.post('/decode', koaBody(), (ctx) => {
+  const body = ctx.request.body
+  if (!body.input) ctx.throw(400, '.input is required')
+  ctx.body = decoder.decode(body.input)
+})
+
+const server = app.listen(3000)
+module.exports = server
