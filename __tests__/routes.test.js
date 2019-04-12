e588e3a77c7d71f8caf878a53befd1714ffa7956
Elin Lütz <elin@freya.se>
2019-04-12 09:21:12 +0200

    Make test more strict

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -22,9 +22,9 @@ describe('Basic route tests', () => {
 
 describe('POST /encode', function () {
   test('posting an input sentence', async () => {
-    const response = await request(server).post('/encode').send({ input: 'test' })
+    const response = await request(server).post('/encode').send({ input: 'Test' })
     expect(response.status).toEqual(200)
-    expect(response.text).toContain('totesostot')
+    expect(response.text).toEqual('Totesostot')
   })
   test('input sentence missing in body', async () => {
     const response = await request(server).post('/encode').send({ unvalidinput: '' })
@@ -35,14 +35,14 @@ describe('POST /encode', function () {
 
 describe('POST /decode', function () {
   test('posting an input sentence to be decoded', async () => {
-    const response = await request(server).post('/decode').send({ input: 'totesostot' })
+    const response = await request(server).post('/decode').send({ input: 'Totesostot' })
     expect(response.status).toEqual(200)
-    expect(response.text).toContain('test')
+    expect(response.text).toEqual('Test')
   })
   test('posting input sentence that is not robber language', async () => {
     const response = await request(server).post('/decode').send({ input: 'testotest' })
     expect(response.status).toEqual(200)
-    expect(response.text).toContain('not decodable')
+    expect(response.text).toEqual('Your sentence is not decodable. Try again.')
   })
   test('input sentence missing in body', async () => {
     const response = await request(server).post('/encode').send({ unvalidinput: '' })
